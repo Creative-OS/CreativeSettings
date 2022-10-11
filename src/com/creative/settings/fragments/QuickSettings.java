1c6db242124f98b568e886a439c820b6b5da7513
minaripenguin37 <alexfinhart@gmail.com>
2022-10-11 12:30:49 +0800

    creative: Bring back the category cards
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- src/com/creative/settings/fragments/QuickSettings.java
+++ src/com/creative/settings/fragments/QuickSettings.java
@@ -75,91 +75,41 @@ import java.util.List;
 import java.util.ArrayList;
 
 @SearchIndexable
-public class Customizations extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class QuickSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    public static final String TAG = "Customizations";
-    
-    private static final String LOCKSCREEN_CATEGORY = "lockscreen_category";
-    private static final String MISC_CATEGORY = "misc_category";
-    private static final String NOTIF_CATEGORY = "notifications_category";
-    private static final String KEY_UDFPS_SETTINGS = "udfps_settings";
-    private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
-    private static final String KEY_FP_AUTH = "fp_success_vibrate";
-    private static final String KEY_FP_ERROR = "fp_error_vibrate";
-    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
+    public static final String TAG = "QuickSettings";
+
     private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
     private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
-    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
     private static final String QUICK_PULLDOWN = "qs_quick_pulldown";
-    private static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String KEY_STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
     private static final String QS_PANEL_STYLE  = "qs_panel_style";
-    private static final String SMART_CHARGING = "smart_charging";
-    private static final String POCKET_JUDGE = "pocket_judge";
-    private static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
-    private static final String SETTINGS_HOMEPAGE_MESSAGES = "settings_homepage_messages";
-    private static final String USE_STOCK_LAYOUT = "use_stock_layout";
-    private static final String UDFPS_HAPTIC_FEEDBACK = "udfps_haptic_feedback";
-    private static final String SMART_PIXELS = "smart_pixels";
     private static final String KEY_SYS_INFO = "qs_system_info";
-    private static final String KEY_VIBRATE_CATEGORY = "incall_vib_options";
-    private static final String KEY_VIBRATE_CONNECT = "vibrate_on_connect";
-    private static final String KEY_VIBRATE_CALLWAITING = "vibrate_on_callwaiting";
-    private static final String KEY_VIBRATE_DISCONNECT = "vibrate_on_disconnect";
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
     private static final int PULLDOWN_DIR_LEFT = 2;
     private static final int PULLDOWN_DIR_ALWAYS = 3;
 
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
-
     private Handler mHandler;
     private IOverlayManager mOverlayManager;
     private IOverlayManager mOverlayService;
-    private Preference mSmartCharging;
-    private SwitchPreference mPocketJudge;
     private SystemSettingListPreference mQsStyle;
     private LineageSecureSettingListPreference mShowBrightnessSlider;
     private LineageSecureSettingListPreference mBrightnessSliderPosition;
     private LineageSecureSettingSwitchPreference mShowAutoBrightness;
-    private LineageSystemSettingListPreference mStatusBarClock;
     private LineageSystemSettingListPreference mQuickPulldown;
-    private SystemSettingListPreference mBatteryPercent;
-    private SystemSettingListPreference mBatteryStyle;
-    private SystemSettingSwitchPreference mShowFourg;
-    private SystemSettingSwitchPreference mOldMobileType;
-    private SystemSettingSwitchPreference mBatteryTextCharging;
-    private Preference mAlertSlider;
-    private SystemSettingListPreference mSettingsDashBoardStyle;
-    private SystemSettingSwitchPreference mSettingsMessages;
-    private SystemSettingSwitchPreference mUseStockLayout;
-    private Preference mUdfpsSettings;
-    private SwitchPreference mRippleEffect;
-    private SwitchPreference mFPVibAuth;
-    private SwitchPreference mFPVibError;
-    private Preference mSmartPixels;
     private ListPreference mSystemInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.creative_customizations);
+        addPreferencesFromResource(R.xml.creative_quicksettings);
 
 	Context mContext = getActivity().getApplicationContext();
 	ContentResolver resolver = mContext.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-
-	final PreferenceCategory vibCategory = prefScreen.findPreference(KEY_VIBRATE_CATEGORY);
-
-        if (!TelephonyUtils.isVoiceCapable(getActivity())) {
-            prefScreen.removePreference(vibCategory);
-        }
         
         mShowBrightnessSlider =
                 (LineageSecureSettingListPreference) findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
@@ -175,23 +125,6 @@ public class Customizations extends SettingsPreferenceFragment implements OnPref
                 (LineageSecureSettingSwitchPreference) findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
             mShowAutoBrightness.setEnabled(showSlider);
 
-        mStatusBarClock =
-                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
-
-        // Adjust status bar preferences for RTL
-        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-            if (DeviceUtils.hasCenteredCutout(mContext)) {
-                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch_rtl);
-                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch_rtl);
-            } else {
-                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
-                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
-            }
-        } else if (DeviceUtils.hasCenteredCutout(mContext)) {
-            mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch);
-            mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
-        }
-
         mQuickPulldown =
                 (LineageSystemSettingListPreference) findPreference(QUICK_PULLDOWN);
         mQuickPulldown.setOnPreferenceChangeListener(this);
@@ -203,64 +136,11 @@ public class Customizations extends SettingsPreferenceFragment implements OnPref
             mQuickPulldown.setEntryValues(R.array.status_bar_quick_qs_pulldown_values_rtl);
         }
 
-        mSettingsDashBoardStyle = (SystemSettingListPreference) findPreference(SETTINGS_DASHBOARD_STYLE);
-        mSettingsDashBoardStyle.setOnPreferenceChangeListener(this);
-        mSettingsMessages = (SystemSettingSwitchPreference) findPreference(SETTINGS_HOMEPAGE_MESSAGES);
-        mSettingsMessages.setOnPreferenceChangeListener(this);
-        mUseStockLayout = (SystemSettingSwitchPreference) findPreference(USE_STOCK_LAYOUT);
-        mUseStockLayout.setOnPreferenceChangeListener(this);
-
         mOverlayService = IOverlayManager.Stub
         .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
 
         mQsStyle = (SystemSettingListPreference) findPreference(QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
-
-        PreferenceCategory lockscreenCategory = (PreferenceCategory) findPreference(LOCKSCREEN_CATEGORY);
-        PreferenceCategory miscCategory = (PreferenceCategory) findPreference(MISC_CATEGORY);
-        PreferenceCategory notifCategory = (PreferenceCategory) findPreference(NOTIF_CATEGORY);
-
-        FingerprintManager mFingerprintManager = (FingerprintManager)
-                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mUdfpsSettings = findPreference(KEY_UDFPS_SETTINGS);
-        mRippleEffect = findPreference(KEY_RIPPLE_EFFECT);
-        mFPVibAuth = findPreference(KEY_FP_AUTH);
-        mFPVibError = findPreference(KEY_FP_ERROR);
-        
-        if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
-            lockscreenCategory.removePreference(mUdfpsSettings);
-            lockscreenCategory.removePreference(mRippleEffect);
-            lockscreenCategory.removePreference(mFPVibAuth);
-            lockscreenCategory.removePreference(mFPVibError);
-        } else {
-            if (!CreativeUtils.isPackageInstalled(getContext(), "com.creative.udfps.icons")) {
-                lockscreenCategory.removePreference(mUdfpsSettings);
-            }
-        }
-        
-        boolean mAlertSliderSupported = getResources().getBoolean(
-                com.android.internal.R.bool.config_hasAlertSlider);
-	mAlertSlider = findPreference(ALERT_SLIDER_PREF);
-        if (!mAlertSliderSupported)
-           notifCategory.removePreference(mAlertSlider);
-        
-        boolean mSmartChargingSupported = getResources().getBoolean(
-                com.android.internal.R.bool.config_smartChargingAvailable);
-	mSmartCharging = findPreference(SMART_CHARGING);
-	if (!mSmartChargingSupported)
-	miscCategory.removePreference(mSmartCharging);
-	
-        boolean mPocketJudgeSupported = getResources().getBoolean(
-                com.android.internal.R.bool.config_pocketModeSupported);
-	mPocketJudge= findPreference(POCKET_JUDGE);
-	if (!mPocketJudgeSupported)
-	miscCategory.removePreference(mPocketJudge);
-	
-        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
-        boolean mSmartPixelsSupported = getResources().getBoolean(
-                com.android.internal.R.bool.config_supportSmartPixels);
-        if (!mSmartPixelsSupported)
-            prefScreen.removePreference(mSmartPixels);
 
         mSystemInfo = (ListPreference) findPreference(KEY_SYS_INFO);
         boolean mSystemInfoSupported = mContext.getResources().getBoolean(
@@ -307,17 +187,31 @@ public class Customizations extends SettingsPreferenceFragment implements OnPref
         } else if (preference == mQsStyle) {
             mCustomSettingsObserver.observe();
             return true;
-        } else if (preference == mSettingsDashBoardStyle) {
-           CreativeUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mSettingsMessages) {
-            CreativeUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mUseStockLayout) {
-            CreativeUtils.showSettingsRestartDialog(getContext());
-            return true;
         }
         return false;
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        String summary="";
+        switch (value) {
+            case PULLDOWN_DIR_NONE:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_qs_pulldown_off);
+                break;
+            case PULLDOWN_DIR_ALWAYS:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_qs_pulldown_always);
+                break;
+            case PULLDOWN_DIR_LEFT:
+            case PULLDOWN_DIR_RIGHT:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_qs_pulldown_summary,
+                    getResources().getString(value == PULLDOWN_DIR_LEFT
+                        ? R.string.status_bar_quick_qs_pulldown_summary_left
+                        : R.string.status_bar_quick_qs_pulldown_summary_right));
+                break;
+        }
+        mQuickPulldown.setSummary(summary);
     }
 
     private void updateQsStyle() {
@@ -367,29 +261,6 @@ public class Customizations extends SettingsPreferenceFragment implements OnPref
         "com.android.system.qs.twotoneaccent"
     };
 
-    private void updateQuickPulldownSummary(int value) {
-        String summary="";
-        switch (value) {
-            case PULLDOWN_DIR_NONE:
-                summary = getResources().getString(
-                    R.string.status_bar_quick_qs_pulldown_off);
-                break;
-            case PULLDOWN_DIR_ALWAYS:
-                summary = getResources().getString(
-                    R.string.status_bar_quick_qs_pulldown_always);
-                break;
-            case PULLDOWN_DIR_LEFT:
-            case PULLDOWN_DIR_RIGHT:
-                summary = getResources().getString(
-                    R.string.status_bar_quick_qs_pulldown_summary,
-                    getResources().getString(value == PULLDOWN_DIR_LEFT
-                        ? R.string.status_bar_quick_qs_pulldown_summary_left
-                        : R.string.status_bar_quick_qs_pulldown_summary_right));
-                break;
-        }
-        mQuickPulldown.setSummary(summary);
-    }
-
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CREATIVE_SETTINGS;
@@ -399,23 +270,16 @@ public class Customizations extends SettingsPreferenceFragment implements OnPref
      * For search
      */
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.creative_customizations) {
+            new BaseSearchIndexProvider(R.xml.creative_quicksettings) {
 
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
-
-                    if (!TelephonyUtils.isVoiceCapable(context)) {
-                        keys.add(KEY_VIBRATE_CATEGORY);
-                        keys.add(KEY_VIBRATE_CONNECT);
-                        keys.add(KEY_VIBRATE_CALLWAITING);
-                        keys.add(KEY_VIBRATE_DISCONNECT);
-                    }
-
-                    boolean mSmartPixelsSupported = context.getResources().getBoolean(
-                            com.android.internal.R.bool.config_supportSmartPixels);
-                    if (!mSmartPixelsSupported)
-                        keys.add(SMART_PIXELS);
+            
+                    boolean mSystemInfoSupported = context.getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportSystemInfo);
+                    if (!mSystemInfoSupported)
+                        keys.add(KEY_SYS_INFO);
 
                     return keys;
 
