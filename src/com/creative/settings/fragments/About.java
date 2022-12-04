0891d070c9350f9fc8891d2d4da67c576ebacd7b
Alvin Francis <nivlafx@gmail.com>
2022-12-04 05:59:15 -0400

    creativedroid to CreativeOS

--- src/com/creative/settings/fragments/About.java
+++ src/com/creative/settings/fragments/About.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2020 CreativeDroid Android Project
+ * Copyright (C) 2016-2020 CreativeOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
